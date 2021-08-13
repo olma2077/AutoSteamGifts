dd10978c3450e0cbe6cdb73edec3bd8187f15d46
Oleg Matviichuk <ma7hway@gmail.com>
2021-08-13 11:07:46 +0300

    Fixed function import

--- autosg/tgbot/handlers/__init__.py
+++ autosg/tgbot/handlers/__init__.py
@@ -3,7 +3,7 @@ Helper modules to handle user interactions with user on Telegram bot.
 """
 from .messages import register_commands
 from .callbacks import register_callbacks
-from .notifications import notify_user
+from .notifications import notify_on_enter
 
 
-__all__ = ['register_commands', 'register_callbacks', 'notify_user']
+__all__ = ['register_commands', 'register_callbacks', 'notify_on_enter']
