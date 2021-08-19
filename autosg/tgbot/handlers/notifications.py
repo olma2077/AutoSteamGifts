20ab8cf4e6497c10f611054078c35df021f7ba08
Oleg Matviichuk <ma7hway@gmail.com>
2021-08-19 13:04:15 +0300

    Notification formatting

--- autosg/tgbot/handlers/notifications.py
+++ autosg/tgbot/handlers/notifications.py
@@ -6,4 +6,4 @@ async def notify_on_enter(user_id: str, game: str, points: int):
     '''Notify user when entered a giveaway'''
     await config.bot.send_message(
         user_id,
-        f'Just entered giveaway of {game}. {points} points left.')
+        f'Just entered giveaway of {game}.\n{points} points left.')
